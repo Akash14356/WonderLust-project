b8636a32496a077351f9cc186d0746e441364790
Akash14356 <123978197+Akash14356@users.noreply.github.com>
2024-06-06 16:31:50 +0530

    "first commit"

---
+++ app.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");   //ejsmate package
+const wrapAsync = require("./utils/wrapAsync.js");
+const  ExpressError= require("./utils/ExpressError.js");
+const listingSchema =require("./schema.js");    //Use Joi NPM Package
+
+const MONGO_url = "mongodb://127.0.0.1:27017/akash";
+main().then(()=>{
+    console.log("connected to db");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_url);
+}
+
+app.set("view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+app.get("/", (req,res)=>{
+    // res.send("Hi, I am root ");
+    res.render("./listings/home.ejs")
+});
+
+const validateListing =(req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    let errMsg= error.details.map((el)=>el.message).join(",");
+    if(error)
+    {
+        throw new ExpressError(400, errMsg);
+    }
+    else{
+        next();
+    }
+};
+
+
+//Index route...
+
+app.get("/listings", wrapAsync(async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings });
+}));
+
+//NEw route
+
+app.get("/listings/new", (req,res)=>{
+    res.render("./listings/new.ejs");
+});
+// app.get("/",(req,res)=>{
+//     res.direst("./listings/show/ejs")
+// });
+
+//SHOW ROUTE..
+
+app.get("/listings/:id", wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs",{listing});
+}));
+
+//create route
+
+app.post("/listings",
+validateListing,
+wrapAsync(async(req,res,next)=>{
+    
+        // let {title,description,image,price,country,location} = req.body;
+    //let listing = req.body.listing;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");    
+    
+
+}));
+
+//Edit route
+
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing});
+}));
+
+//update route
+
+app.put("/listings/:id",
+validateListing,
+wrapAsync( async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+}));
+
+
+//delete route
+
+app.delete("/listings/:id", wrapAsync(async(req,res)=>{
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+
+}));
+
+// app.get("/testListing", async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "my new Villa",
+//         description : "By the beach ",
+//         price : 1200,
+//         location : "Calangute, Goa",
+//         country : "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample was saved ");
+//     res.send("Successful  testing");
+// });
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
+})
+
+
+app.use("/",(err,req,res,next)=>{
+    let{statusCode =500, message="Something went wrong!"}=err;
+    res.status(statusCode).render("error.ejs",{err});
+    // res.status(statusCode).send(message);
+})
+
+
+
+
+app.listen(8080, ()=>{
+    console.log("server is listening to port 8080");
+});
